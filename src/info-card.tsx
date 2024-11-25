4322c9137c45f4740c7224ededfb114b0acba27b
Vladimir Gajčin <djvlada03@gmail.com>
2024-11-25 12:23:01 +0100

    Drop shadow on info card main image

--- src/info-card.tsx
+++ src/info-card.tsx
@@ -15,11 +15,11 @@ function InfoCard({ openCategory, lsdOpen }) {
     const setOpacityOnChild = true;
     return (
         <div className="card-container" style={{ filter: 'drop-shadow(rgba(0,0,0,0.7) 0px 0px 10px)', margin: 0, opacity: setOpacityOnChild ? 1 : 0.8 }}>
-            <div className="card" style={{ backgroundColor: setOpacityOnChild ? 'rgba(255, 255, 255, 0.9)' : 'white', clipPath: polygon, display: 'flex', flexDirection: 'column', width: lsdOpen ? '380px' : '300px', height: 'fit-content', margin: lsdOpen ? 0 : '0 20px' }}>
+            <div className="card" style={{ backgroundColor: !setOpacityOnChild ? 'rgba(255, 255, 255, 0.9)' : 'white', clipPath: polygon, display: 'flex', flexDirection: 'column', width: lsdOpen ? '380px' : '300px', height: 'fit-content', margin: lsdOpen ? 0 : '0 20px' }}>
                 <h2 className="card-header" style={{ textTransform: 'uppercase', fontStyle: 'italic', backgroundColor: category?.color, color: 'white', marginBottom: 0, padding: '10px 20px', marginTop: '30px' }}><b>{category?.title}<br />{category?.subtitle}</b></h2>
                 {
                     img
-                        ? <img src={img} alt="Avatar" style={{ width: '100%' }} />
+                        ? <img src={img} alt="Avatar" style={{ width: '100%', boxShadow: '0 20px 20px -25px black' }} />
                         : <></>
                 }
                 <div className="container" style={{ fontFamily: 'JosefinSans', fontWeight: '700', margin: '0 10px', opacity: '0.8', padding: '10px 20px 0 20px' }}>
